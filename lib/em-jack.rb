438f0ce9defa675ae6df5642ce74c26bcefbb707
Michael Pye <mail@mdpye.co.uk>
2017-11-13 11:16:26 +0000

    Register handlers in rough priority order

--- lib/em-jack.rb
+++ lib/em-jack.rb
@@ -6,9 +6,19 @@ require 'em-jack/beanstalk_connection'
 require 'em-jack/connection'
 require 'em-jack/version'
 
-Dir["#{File.dirname(__FILE__)}/em-jack/handlers/*.rb"].each do |file|
-  require file
-end
+require 'em-jack/handlers/reserved'
+require 'em-jack/handlers/inserted'
+require 'em-jack/handlers/deleted'
+require 'em-jack/handlers/ok'
+require 'em-jack/handlers/released'
+require 'em-jack/handlers/buried'
+require 'em-jack/handlers/touched'
+require 'em-jack/handlers/errors'
+require 'em-jack/handlers/kicked'
+require 'em-jack/handlers/not_ignored'
+require 'em-jack/handlers/paused'
+require 'em-jack/handlers/using'
+require 'em-jack/handlers/watching'
 
 module EMJack
 end
